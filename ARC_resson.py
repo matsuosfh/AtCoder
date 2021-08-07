4bdd63dddaa413ab06755001e0d2dd84a579df56
matsuosfh <matsuosfh@gmail.com>
2021-08-08 01:40:17 +0900

    Default Changelist

--- ARC_resson.py
+++
@@ -1,9 +0,0 @@
-#AtCoder Regular Contest 109
-#A - Hands
-
-#A - Hands
-a,b,x,y= map(int,input().split())
-
-h = abs(b-a)
-ans = min(x*h+y,y*(h-1)+x,y*h+x)
-print(ans)
--- gcd.py
+++
@@ -1,9 +0,0 @@
-def gcd(m,n):
-    if n == 0 :
-        return m
-    return gcd(n, m % n)
-
-
-N,K= map(int,input().split())
-
-print(gcd(N,K))
